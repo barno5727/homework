4f1cb37818d907c0284618f9e57df2ecdc21703f
unknown <barnoodilovna7405727@mail.ru>
2022-11-20 11:28:35 +0500

    homework-js

---
+++ app.js
@@ -0,0 +1,10 @@
+alert("Enter Your Autobiography")
+prompt ("Enter Your First Name:");
+prompt ("Enter Your Last Name:");
+
+confirm("Are You is Married?")
+prompt ("Enter Your Age:");
+prompt ("Enter Your Adress:");
+prompt ("Enter Your Email Adress:");
+prompt ("Enter Your Profession:");
+prompt ("Enter Your Hobby:");
